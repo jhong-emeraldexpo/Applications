e44a56ea201134a4b0d2a5f21d66d8a9439d3265
James Hong <james.hong@emeraldexpo.com>
2018-03-02 00:16:36 -0800

    test changes

--- Data Mart/docs/fileUtil.bat
+++ Data Mart/docs/fileUtil.bat
@@ -2,3 +2,4 @@
 forfiles /p "." /s /m "*.*" /d -4 /c "cmd /c dir @path"
 forfiles /p "C:\Windows\Temp" /s /m "*.*" /d -6 /c "cmd /c dir @path"
 Rem forfiles /p "C:\Windows\Temp" /s /m "*.*" /d -6 /c "cmd /c del @path" >> deletedfiles.txt
+Rem Delete files
