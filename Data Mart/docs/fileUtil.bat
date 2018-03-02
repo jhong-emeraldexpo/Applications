b6d6ceb576a1a1323f6ebc207b386feae64357eb
Hong <jahong@EMERALDEXPO.NET>
2018-03-01 23:29:33 -0800

    Add new files

---
+++ Data Mart/docs/fileUtil.bat
@@ -0,0 +1,4 @@
+
+forfiles /p "." /s /m "*.*" /d -4 /c "cmd /c dir @path"
+forfiles /p "C:\Windows\Temp" /s /m "*.*" /d -6 /c "cmd /c dir @path"
+Rem forfiles /p "C:\Windows\Temp" /s /m "*.*" /d -6 /c "cmd /c del @path" >> deletedfiles.txt
